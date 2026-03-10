39668d079b0aa17ad480f0b95cde9eb84e317c16
aliakseividaseu <alexaat@yahoo.co.uk>
2026-03-10 20:43:47 +0000

    update vercel deployment

--- api/index.go
+++ api/index.go
@@ -1,22 +1,47 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+
+	. "github.com/tbxark/g4vercel"
 )
 
-// func main() {
-
-// 	http.HandleFunc("/", formHandler)
-
-// 	const portNumber = ":8080"
-// 	err := http.ListenAndServe(portNumber, nil)
-// 	if err != nil {
-// 		fmt.Println("\nCannot start server")
-// 	}
-// }
-
 func Handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Go on Vercel!"))
+	server := New()
+
+	server.GET("/", func(context *Context) {
+		context.JSON(200, H{
+			"message": "hello go from vercel !!!!",
+		})
+	})
+	server.GET("/hello", func(context *Context) {
+		name := context.Query("name")
+		if name == "" {
+			context.JSON(400, H{
+				"message": "name not found",
+			})
+		} else {
+			context.JSON(200, H{
+				"data": fmt.Sprintf("Hello %s!", name),
+			})
+		}
+	})
+	server.GET("/user/:id", func(context *Context) {
+		context.JSON(400, H{
+			"data": H{
+				"id": context.Param("id"),
+			},
+		})
+	})
+	server.GET("/long/long/long/path/*test", func(context *Context) {
+		context.JSON(200, H{
+			"data": H{
+				"url": context.Path,
+			},
+		})
+	})
+	server.Handle(w, r)
 }
 
 /*
