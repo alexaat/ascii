bfa97f1ec97639b1b9ea92495cf0e8985d943ad2
aliakseividaseu <alexaat@yahoo.co.uk>
2026-03-10 21:08:43 +0000

    deployment update

--- api/io.go
+++ api/io.go
@@ -1,24 +1,24 @@
 package handler
 
-import (
-	"os"
-)
+// import (
+// 	"os"
+// )
 
-var (
-	filePath          = "data.txt"
-	errorTemplatePath = "templates/error.html"
-	templatePath      = "templates/index.html"
-	zipFilePath       = "archive.zip"
-)
+// var (
+// 	filePath          = "data.txt"
+// 	errorTemplatePath = "templates/error.html"
+// 	templatePath      = "templates/index.html"
+// 	zipFilePath       = "archive.zip"
+// )
 
-func readFile(s string) (string, error) {
-	data, err := os.ReadFile(s)
-	if err != nil {
-		return "", err
-	}
-	return string(data), nil
-}
+// func readFile(s string) (string, error) {
+// 	data, err := os.ReadFile(s)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return string(data), nil
+// }
 
-func writeToFile(fileName string, data []byte) error {
-	return os.WriteFile(fileName, data, 0644)
-}
+// func writeToFile(fileName string, data []byte) error {
+// 	return os.WriteFile(fileName, data, 0644)
+// }
