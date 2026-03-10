bfe5e945a249e363d978724986125e157c3d8aae
aliakseividaseu <alexaat@yahoo.co.uk>
2026-03-10 21:13:22 +0000

    deployment update

--- api/io.go
+++ api/io.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"os"
+	//"os"
 )
 
 var (
@@ -11,14 +11,14 @@ var (
 	ZipFilePath       = "archive.zip"
 )
 
-func ReadFile(s string) (string, error) {
-	data, err := os.ReadFile(s)
-	if err != nil {
-		return "", err
-	}
-	return string(data), nil
-}
+// func ReadFile(s string) (string, error) {
+// 	data, err := os.ReadFile(s)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return string(data), nil
+// }
 
-func WriteToFile(fileName string, data []byte) error {
-	return os.WriteFile(fileName, data, 0644)
-}
+// func WriteToFile(fileName string, data []byte) error {
+// 	return os.WriteFile(fileName, data, 0644)
+// }
