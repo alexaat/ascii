8d2e0684368eb14b962c062b6698bab95f625d11
aliakseividaseu <alexaat@yahoo.co.uk>
2026-03-13 20:36:27 +0000

    deployment update

--- api/index.go
+++ api/index.go
@@ -9,10 +9,11 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 //go:embed templates/*.html banners/*.txt
-var templates embed.FS
+var assets embed.FS
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
@@ -41,7 +42,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 func formHandler(w http.ResponseWriter) {
 	// Render the index.html template
-	tmpl, err := template.ParseFS(templates, "templates/index.html")
+	tmpl, err := template.ParseFS(assets, "templates/index.html")
 	if err != nil {
 		showError(w, "500 TEMPLATE NOT FOUND", http.StatusInternalServerError)
 		return
@@ -59,7 +60,7 @@ func resultHandler(w http.ResponseWriter, r *http.Request) {
 	banner := r.FormValue("banner")
 	text := r.FormValue("request")
 	bannerUrl := "banners/" + banner + ".txt"
-	b, err := utils.ReadFile(bannerUrl, templates)
+	b, err := utils.ReadFile(bannerUrl, assets)
 	if err != nil {
 		showError(w, "505 BANNER NOT FOUND: "+bannerUrl, http.StatusInternalServerError)
 		return
@@ -76,13 +77,7 @@ func resultHandler(w http.ResponseWriter, r *http.Request) {
 
 	createZip(w)
 
-	// err = utils.WriteToFile(utils.FilePath, []byte(result))
-	// if err != nil {
-	// 	showError(w, "500 Cannot write to file", http.StatusInternalServerError)
-	// 	return
-	// }
-
-	tmpl, err := template.ParseFS(templates, "templates/result.html")
+	tmpl, err := template.ParseFS(assets, "templates/result.html")
 	if err != nil {
 		showError(w, "500 TEMPLATE NOT FOUND", http.StatusInternalServerError)
 		return
@@ -99,11 +94,21 @@ func downloadHandler(w http.ResponseWriter, r *http.Request) {
 	format := r.FormValue("format")
 	switch format {
 	case "zip":
-		//createZip(w)
+		info, err := os.Stat(utils.ZipFilePath)
+		if err != nil {
+			size := info.Size()
+			w.Header().Set("Content-Length", strconv.Itoa(int(size)))
+		}
 		w.Header().Set("Content-Disposition", "attachment; filename="+utils.ZipFilePath)
 		w.Header().Set("Content-Type", "application/zip")
+
 		http.ServeFile(w, r, utils.ZipFilePath)
 	case "text":
+		info, err := os.Stat(utils.FilePath)
+		if err != nil {
+			size := info.Size()
+			w.Header().Set("Content-Length", strconv.Itoa(int(size)))
+		}
 		w.Header().Set("Content-Disposition", "attachment; filename="+utils.FilePath)
 		w.Header().Set("Content-Type", "text/plain")
 		http.ServeFile(w, r, utils.FilePath)
@@ -145,7 +150,7 @@ func createZip(w http.ResponseWriter) {
 
 // Render the error.html template
 func showError(w http.ResponseWriter, message string, statusCode int) {
-	tmpl, err := template.ParseFS(templates, "templates/error.html")
+	tmpl, err := template.ParseFS(assets, "templates/error.html")
 	if err == nil {
 		w.WriteHeader(statusCode)
 		err := tmpl.Execute(w, message)
@@ -162,92 +167,3 @@ func showError(w http.ResponseWriter, message string, statusCode int) {
 		fmt.Fprintf(w, err.Error())
 	}
 }
-
-// func createZip(w http.ResponseWriter) {
-// 	archive, err := os.Create(ZipFilePath)
-// 	if err != nil {
-// 		showError(w, "500 INTERNAL SERVER ERROR", http.StatusInternalServerError)
-// 		return
-// 	}
-// 	defer archive.Close()
-
-// 	zipWriter := zip.NewWriter(archive)
-
-// 	f1, err := os.Open(FilePath)
-// 	if err != nil {
-// 		showError(w, "500 INTERNAL SERVER ERROR", http.StatusInternalServerError)
-// 		return
-// 	}
-// 	defer f1.Close()
-
-// 	w1, err := zipWriter.Create(FilePath)
-// 	if err != nil {
-// 		showError(w, "500 INTERNAL SERVER ERROR", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	if _, err := io.Copy(w1, f1); err != nil {
-// 		showError(w, "500 INTERNAL SERVER ERROR", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	zipWriter.Close()
-// }
-
-/*
-package handler
-
-import (
-	//"archive/zip"
-	"fmt"
-	//"io"
-	"net/http"
-	//"os"
-	//"strconv"
-	"text/template"
-)
-
-var (
-	filePath          = "data.txt"
-	errorTemplatePath = "templates/error.html"
-	templatePath      = "/ascii/templates/index.html"
-	zipFilePath       = "archive.zip"
-)
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path == "/" {
-		formHandler(w, r)
-	}
-
-	//fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-}
-
-func formHandler(w http.ResponseWriter, r *http.Request) {
-
-	// Render the index.html template
-	t, err := template.ParseFiles(templatePath)
-	if err != nil {
-		showError(w, "404 TEMPLATE NOT FOUND: "+err.Error(), http.StatusNotFound)
-		return
-	}
-	err = t.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		showError(w, "500 INTERNAL SERVER ERROR: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	//fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-}
-
-// Render the error.html template
-func showError(w http.ResponseWriter, message string, statusCode int) {
-	fmt.Fprintf(w, "<h1>ERROR </h1>")
-	fmt.Fprintf(w, message)
-	// t, err := template.ParseFiles(errorTemplatePath)
-	// if err == nil {
-	// 	w.WriteHeader(statusCode)
-	// 	t.Execute(w, message)
-	// }
-}
-*/
